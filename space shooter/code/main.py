df6eed942929dde9f56a7c308e8ed115a57799b3
Maxim Sorkin <maximsorkin@cmail.carleton.ca>
2026-05-18 01:13:21 -0400

    Add player image and update background color in main game loop

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -7,6 +7,14 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
 
+# plain surface
+surf = pygame.Surface((100,200))
+surf.fill('orange')
+x = 100
+
+# importing an image
+player_surf = pygame.image.load('../images/player.png')
+
 while running:
     # event loop
     for event in pygame.event.get():
@@ -14,7 +22,9 @@ while running:
             running = False
 
     # draw the game
-    display_surface.fill('hotpink')
+    display_surface.fill('darkgray')
+    x += 0.1
+    display_surface.blit(player_surf, (x,150))
     pygame.display.update()
 
 
