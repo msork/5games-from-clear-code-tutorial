0d789ecf5dfc90e0cee8f05f5d05f97acc835106
Maxim Sorkin <maximsorkin@cmail.carleton.ca>
2026-05-18 22:23:19 -0400

    Add meteor and laser images, implement player movement logic

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -15,12 +15,19 @@ surf = pygame.Surface((100,200))
 surf.fill('orange')
 x = 100
 
-# importing an image
+# imports
 player_surf = pygame.image.load(join('..', 'images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2,WINDOW_HEIGHT / 2))
+player_direction = 1
 
 star_surf = pygame.image.load(join('..', 'images', 'star.png')).convert_alpha()
-
 stars_pos = [(randint(0,WINDOW_WIDTH), randint(0,WINDOW_HEIGHT)) for i in range (20)]
+
+meteor_surf = pygame.image.load(join('..', 'images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join('..', 'images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
     # event loop
@@ -32,9 +39,15 @@ while running:
     display_surface.fill('darkgray')
     for star_pos in stars_pos:
         display_surface.blit(star_surf, star_pos)
-    x += 0.1
-    display_surface.blit(player_surf, (x,150))
 
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # player movement
+    player_rect.left += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf, player_rect.topleft)
 
     pygame.display.update()
 
